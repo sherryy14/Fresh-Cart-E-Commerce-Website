00508d93f696d64d29e5001bee525299fd06a785
Muhammad Shahryar <shahryar@aptech.com>
2023-05-22 13:33:03 +0500

    index page

---
+++ index.php
@@ -0,0 +1,413 @@
+<?php
+include "header.php";
+include "config.php";
+
+?>
+
+<main>
+  <section class="mt-8">
+    <div class="container">
+      <div class="hero-slider ">
+        <div style="background: url(assets/images/slider/slide-1.jpg)no-repeat; background-size: cover; border-radius: .5rem; background-position: center;">
+          <div class="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center">
+            <span class="badge text-bg-warning">Opening Sale Discount 50%</span>
+
+            <h2 class="text-dark display-5 fw-bold mt-4">SuperMarket For Fresh Grocery </h2>
+            <p class="lead">Introduced a new model for online grocery shopping
+              and convenient home delivery.</p>
+            <a href="#!" class="btn btn-dark mt-3">Shop Now <i class="feather-icon icon-arrow-right ms-1"></i></a>
+          </div>
+
+        </div>
+        <div class=" " style="background: url(assets/images/slider/slider-2.jpg)no-repeat; background-size: cover; border-radius: .5rem; background-position: center;">
+          <div class="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center">
+            <span class="badge text-bg-warning">Free Shipping - orders over $100</span>
+            <h2 class="text-dark display-5 fw-bold mt-4">Free Shipping on <br> orders over <span class="text-primary">$100</span></h2>
+            <p class="lead">Free Shipping to First-Time Customers Only, After promotions and discounts are applied.
+            </p>
+            <a href="#!" class="btn btn-dark mt-3">Shop Now <i class="feather-icon icon-arrow-right ms-1"></i></a>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!-- Category Section Start-->
+  <section class="mb-lg-10 mt-lg-14 my-8">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mb-6">
+
+          <h3 class="mb-0">Featured Categories</h3>
+
+        </div>
+      </div>
+      <div class="category-slider ">
+
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-dairy-bread-eggs.jpg" alt="Grocery Ecommerce Template" class="mb-3 img-fluid">
+                <div class="text-truncate">Dairy, Bread & Eggs</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-snack-munchies.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Snack & Munchies</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-bakery-biscuits.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Bakery & Biscuits</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-instant-food.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Instant Food</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-tea-coffee-drinks.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Tea, Coffee & Drinks</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"><a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-atta-rice-dal.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Atta, Rice & Dal</div>
+              </div>
+            </div>
+          </a></div>
+
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-baby-care.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Baby Care</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-chicken-meat-fish.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Chicken, Meat & Fish</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-cleaning-essentials.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Cleaning Essentials</div>
+              </div>
+            </div>
+          </a></div>
+        <div class="item"> <a href="pages/shop-grid.html" class="text-decoration-none text-inherit">
+            <div class="card card-product mb-lg-4">
+              <div class="card-body text-center py-8">
+                <img src="assets/images/category/category-pet-care.jpg" alt="Grocery Ecommerce Template" class="mb-3">
+                <div class="text-truncate">Pet Care</div>
+              </div>
+            </div>
+          </a></div>
+
+
+
+
+      </div>
+
+
+    </div>
+  </section>
+  <!-- Category Section End-->
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6 mb-3 mb-lg-0">
+          <div>
+            <div class="py-10 px-8 rounded" style="background:url(assets/images/banner/grocery-banner.png)no-repeat; background-size: cover; background-position: center;">
+              <div>
+                <h3 class="fw-bold mb-1">Fruits & Vegetables
+                </h3>
+                <p class="mb-4">Get Upto <span class="fw-bold">30%</span> Off</p>
+                <a href="#!" class="btn btn-dark">Shop Now</a>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+        <div class="col-12 col-md-6 ">
+
+          <div>
+            <div class="py-10 px-8 rounded" style="background:url(assets/images/banner/grocery-banner-2.jpg)no-repeat; background-size: cover; background-position: center;">
+              <div>
+                <h3 class="fw-bold mb-1">Freshly Baked
+                  Buns
+                </h3>
+                <p class="mb-4">Get Upto <span class="fw-bold">25%</span> Off</p>
+                <a href="#!" class="btn btn-dark">Shop Now</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Popular Products Start-->
+  <section class="my-lg-14 my-8">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mb-6">
+
+          <h3 class="mb-0">Popular Products</h3>
+
+        </div>
+      </div>
+
+      <div class="row g-4 row-cols-lg-5 row-cols-2 row-cols-md-3">
+        <?php
+        // $product = "SELECT "
+        $query = "SELECT * FROM category AS c INNER JOIN products AS p ON c.cat_id = p.p_id";
+        $result = mysqli_query($conn, $query);
+
+        if(mysqli_num_rows($result)>0){
+          while($row = mysqli_fetch_array($result)){ 
+        ?>
+        
+        <!-- Prodcut Card Start  -->
+        <div class="col">
+          <div class="card card-product">
+            <div class="card-body">
+
+              <div class="text-center position-relative">
+                <div class=" position-absolute top-0 start-0">
+                  <span class="badge bg-danger">Sale</span>
+                </div>
+                <a href="#!"> <img src="assets/images/products/<?php echo $row['img1'];?>" alt="<?php echo $row['p_title'];?>" class="mb-3 img-fluid"></a>
+
+                <div class="card-product-action">
+                  <a href="#!" class="btn-action" data-bs-toggle="modal" data-bs-target="#quickViewModal<?php echo $row['p_id'];?>"><i class="bi bi-eye" data-bs-toggle="tooltip" data-bs-html="true" title="Quick View"></i></a>
+                  <a href="#!" class="btn-action" data-bs-toggle="tooltip" data-bs-html="true" title="Wishlist"><i class="bi bi-heart"></i></a>
+                  <a href="#!" class="btn-action" data-bs-toggle="tooltip" data-bs-html="true" title="Compare"><i class="bi bi-arrow-left-right"></i></a>
+                </div>
+
+              </div>
+              <div class="text-small mb-1"><a href="#!" class="text-decoration-none text-muted"><small><?php echo $row['cat_name'];?></small></a></div>
+              
+              <h2 class="fs-6"><a href="pages/shop-single.php?p_id=<?php echo $row['p_id'];?>" class="text-inherit text-decoration-none"><?php echo $row['p_title'];?></a></h2>
+              <div>
+
+                <small class="text-warning"> <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-half"></i></small> <span class="text-muted small">4.5(149)</span>
+              </div>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div><span class="text-dark">$<?php echo $row['p_price'];?></span> <span class="text-decoration-line-through text-muted">$<?php echo $row['re_price'];?></span>
+                </div>
+                <div><a href="#!" class="btn btn-primary btn-sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
+                      <line x1="12" y1="5" x2="12" y2="19"></line>
+                      <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg> Add</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Prodcut Card Start  -->
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="quickViewModal<?php echo $row['p_id'];?>" tabindex="-1" aria-hidden="true">
+          <div class="modal-dialog modal-xl modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-body p-8">
+                <div class="position-absolute top-0 end-0 me-3 mt-3">
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="row">
+                  <div class="col-lg-6">
+                    <!-- img slide -->
+                    <div class="product productModal" id="productModal">
+                      <div class="zoom" onmousemove="zoom(event)" style="
+                  background-image: url(assets/images/products/<?php echo $row['img1'];?>);
+                ">
+                        <!-- img -->
+                        <img src="assets/images/products/<?php echo $row['img1'];?>" alt="">
+                      </div>
+                      <div>
+                        <div class="zoom" onmousemove="zoom(event)" style="
+                    background-image: url(assets/images/products/<?php echo $row['img2'];?>);
+                  ">
+                          <!-- img -->
+                          <img src="assets/images/products/<?php echo $row['img2'];?>" alt="">
+                        </div>
+                      </div>
+                      <div>
+                        <div class="zoom" onmousemove="zoom(event)" style="
+                    background-image: url(assets/images/products/<?php echo $row['img3'];?>);
+                  ">
+                          <!-- img -->
+                          <img src="assets/images/products/<?php echo $row['img3'];?>" alt="">
+                        </div>
+                      </div>
+                      <div>
+                        <div class="zoom" onmousemove="zoom(event)" style="
+                    background-image: url(assets/images/products/<?php echo $row['img4'];?>);
+                  ">
+                          <!-- img -->
+                          <img src="assets/images/products/<?php echo $row['img4'];?>" alt="">
+                        </div>
+                      </div>
+                    </div>
+                    <!-- product tools -->
+                    <div class="product-tools">
+                      <div class="thumbnails row g-3" id="productModalThumbnails">
+                        <div class="col-3" class="tns-nav-active">
+                          <div class="thumbnails-img">
+                            <!-- img -->
+                            <img src="assets/images/products/<?php echo $row['img1'];?>" alt="">
+                          </div>
+                        </div>
+                        <div class="col-3">
+                          <div class="thumbnails-img">
+                            <!-- img -->
+                            <img src="assets/images/products/<?php echo $row['img2'];?>" alt="">
+                          </div>
+                        </div>
+                        <div class="col-3">
+                          <div class="thumbnails-img">
+                            <!-- img -->
+                            <img src="assets/images/products/<?php echo $row['img3'];?>" alt="">
+                          </div>
+                        </div>
+                        <div class="col-3">
+                          <div class="thumbnails-img">
+                            <!-- img -->
+                            <img src="assets/images/products/<?php echo $row['img4'];?>" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="ps-lg-8 mt-6 mt-lg-0">
+                      <a href="#!" class="mb-4 d-block">Bakery Biscuits</a>
+                      <h2 class="mb-1 h1"><?php echo $row['p_title'];?></h2>
+                      <div class="mb-4">
+                        <small class="text-warning">
+                          <i class="bi bi-star-fill"></i>
+                          <i class="bi bi-star-fill"></i>
+                          <i class="bi bi-star-fill"></i>
+                          <i class="bi bi-star-fill"></i>
+                          <i class="bi bi-star-half"></i></small><a href="#" class="ms-2">(30 reviews)</a>
+                      </div>
+                      <div class="fs-4">
+                        <span class="fw-bold text-dark">$<?php echo $row['p_price'];?></span>
+                        <span class="text-decoration-line-through text-muted">$<?php echo $row['re_price'];?></span><span><small class="fs-6 ms-2 text-danger"><?php echo round(($row['p_price']/$row['re_price'])*100);?> % Off</small></span>
+                      </div>
+                      <hr class="my-6">
+                      <div class="mb-4">
+                        <button type="button" class="btn btn-outline-secondary">
+                          250g
+                        </button>
+                        <button type="button" class="btn btn-outline-secondary">
+                          500g
+                        </button>
+                        <button type="button" class="btn btn-outline-secondary">
+                          1kg
+                        </button>
+                      </div>
+                      <div>
+                        <!-- input -->
+                        <!-- input -->
+                        <div class="input-group input-spinner  ">
+                          <input type="button" value="-" class="button-minus  btn  btn-sm " data-field="quantity">
+                          <input type="number" step="1" max="10" value="1" name="quantity" class="quantity-field form-control-sm form-input   ">
+                          <input type="button" value="+" class="button-plus btn btn-sm " data-field="quantity">
+                        </div>
+                      </div>
+                      <div class="mt-3 row justify-content-start g-2 align-items-center">
+
+                        <div class="col-lg-4 col-md-5 col-6 d-grid">
+                          <!-- button -->
+                          <!-- btn -->
+                          <button type="button" class="btn btn-primary">
+                            <i class="feather-icon icon-shopping-bag me-2"></i>Add to
+                            cart
+                          </button>
+                        </div>
+                        <div class="col-md-4 col-5">
+                          <!-- btn -->
+                          <a class="btn btn-light" href="#" data-bs-toggle="tooltip" data-bs-html="true" aria-label="Compare"><i class="bi bi-arrow-left-right"></i></a>
+                          <a class="btn btn-light" href="#!" data-bs-toggle="tooltip" data-bs-html="true" aria-label="Wishlist"><i class="feather-icon icon-heart"></i></a>
+                        </div>
+                      </div>
+                      <hr class="my-6">
+                      <div>
+                        <table class="table table-borderless">
+                          <tbody>
+                            <tr>
+                              <td>Product Code:</td>
+                              <td><?php echo $row['p_code'];?></td>
+                            </tr>
+                            <tr>
+                              <td>Availability:</td>
+                              <td><?php echo $row['stock'];?></td>
+                            </tr>
+                            <tr>
+                              <td>Type:</td>
+                              <td>Fruits</td>
+                            </tr>
+                            <tr>
+                              <td>Shipping:</td>
+                              <td>
+                                <small>01 day shipping.<span class="text-muted">( Free pickup today)</span></small>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Modal End  -->
+
+        <?php 
+         }
+        }
+        ?>
+      </div>
+    </div>
+  </section>
+</main>
+
+
+
+<?php
+include "footer.php";
+?>
